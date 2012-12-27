5fc263b15ece86f15147b9f71f9889d7e1a27693
Joe Kendall <joe@joekendall.co.uk>
2012-12-27 16:54:26 +0000

    Application Upload
    Barebones app uploaded, initial file removed

---
+++ ruby_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyApp::Application.config.secret_token = '631a1d623e041ba6f5c2ba9912d6fdc044b5c9e2e336a03b10c3b9a57eee01a20899f916b1802532d0e28c87eea19dcb2f70477f4c71cdfcfaec08d092dc999b'
