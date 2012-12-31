74be65623c17b1b6a7e2af93d48b3663e502b6f4
Joe Kendall <joe@joekendall.co.uk>
2012-12-31 21:44:45 +0000

    extra bits

--- ruby_app/app/controllers/welcome_controller.rb
+++ ruby_app/app/controllers/welcome_controller.rb
@@ -1,4 +1,11 @@
 class WelcomeController < ApplicationController
-  def index
+  	def index
+  	end
+end
+
+class BlogEntry
+  def initialize(title, mood, fulltext)
+  	@time = Time.now
+    @title, @mood, @fulltext = title, mood, fulltext
   end
 end
